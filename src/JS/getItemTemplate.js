0746c281f1422059de08ab301f402735e69ca612
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-12-10 14:47:44 +0200

    tempete

---
+++ src/JS/getItemTemplate.js
@@ -0,0 +1,29 @@
+export const getItemTemplate = ({
+  webformatURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+  largeImageURL,
+}) => `<div class="photo-card">
+           <a class='photo-card__link' href='${largeImageURL}'><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+       <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          <b>${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          <b>${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          <b>${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          <b>${downloads}</b>
+        </p>
+       </div>
+      </div>`;
